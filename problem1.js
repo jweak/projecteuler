b4d0d4c8a17daf2070893367e8faf8b79988b0f1
Juho Heikka <juho.heikka@gmail.com>
2014-02-18 21:25:12 +0200

    Tabs to spaces. Right file for code for problem2.

--- problem1.js
+++ problem1.js
@@ -1,9 +1,9 @@
-function result() {
-	var sum = 0;
-	for (var i = 0; i < 1000; i++) {
-		if (i % 3 === 0 || i % 5 === 0) {
-			sum += i;
-		}
-	}
-	return sum;
+function problem1() {
+  var sum = 0;
+  for (var i = 0; i < 1000; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
 }
--- problem2.js
+++ problem2.js
@@ -1,28 +1,15 @@
-function result() {
-	var primes = [2,3,5,7,11,13,17,19,23,29,31,37,
-	41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,
-	109,113,127,131,137,139,149,151,157,163,167,173];
-	var num = 600851475143;
-	var foundPrimes = [];
+function problem2() {
+  var fib1 = 1;
+  var fib2 = 2;
+  var sumOfEvenNumbers = 2;
+  while (fib1 < 4000000) {
+    var temp = fib1 + fib2;
+    if (temp % 2 === 0) {
+      sumOfEvenNumbers += temp;
+    }
+    fib1 = fib2;
+    fib2 = temp;
+  }
 
-
-	var findPrimes = function(number) {
-		var retVal = 0;
-		for (var i = 0; i < primes.length; i++) {
-			var result = number / primes[i];
-			if (result === parseInt(result)) {
-				console.log(result);
-				foundPrimes.push(primes[i]);
-				retVal = result;
-				break;
-			}
-		}
-		return retVal;
-	}
-
-	while (num > 173) {
-		num = findPrimes(num);
-	}
-	
-	return foundPrimes;
+  return sumOfEvenNumbers;
 }
