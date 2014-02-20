e871d3074af8fd9da7a38796f6507d1720c7a704
Juho Heikka <juho.heikka@gmail.com>
2014-02-20 22:11:50 +0200

    Solution for problem 5.

---
+++ problem5.js
@@ -0,0 +1,11 @@
+function problem5() {
+  for (var i = 20; i < 1000000000; i += 20) {
+    for (var x = 3; x < 20; x++) {
+      if (i % x !== 0) {
+        break;
+      } else if (x === 19) {
+        return i;
+      }
+    }
+  }
+}
