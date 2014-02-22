2537f92167d4eafbb581a8330f9ca86bb5dd4db7
Juho Heikka <juho.heikka@gmail.com>
2014-02-22 13:31:58 +0200

    Added a solution for problem 6

---
+++ problem6.js
@@ -0,0 +1,22 @@
+function sumOfSquares(min, max) {
+  var sum = 0;
+  for (var i = min; i <= max; i++) {
+    sum += i * i;
+  }
+  return sum;
+} 
+
+function sumFun(min, max) {
+  var sum = 0;
+  for (var i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
+} 
+
+function problem6() {
+  var sum = sumFun(1, 100);
+  var sumSquares = sumOfSquares(1, 100);
+
+  return sum * sum - sumSquares;
+}
